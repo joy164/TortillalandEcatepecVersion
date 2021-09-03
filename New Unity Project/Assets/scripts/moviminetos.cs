0cd86d3227ba0976fe094d6aa1be3b23aedc3a8e
joy164 <joelvillar35@gmail.com>
2021-09-03 13:02:12 -0500

    primeros modelos

---
+++ New Unity Project/Assets/scripts/moviminetos.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moviminetos : MonoBehaviour{
+    public float speed = 10.0f, gravedad = 10.0f;
+    
+    private Rigidbody Physics;
+    // Start is called before the first frame update
+    void Start(){
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Physics = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        float horizontal = Input.GetAxis("Horizontal");
+        
+        
+        transform.Translate(new Vector3(horizontal, 0.0f, 0.0f) * Time.deltaTime * speed);
+        
+        /*float rotacionCamara = Input.GetAxis("Mouse Y");
+        transform.Rotate(new Vector3( 0.0f, 0.0f, rotacionCamara * Time.deltaTime * rotacion));*/
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Physics.AddForce(new Vector3(0,gravedad,0), ForceMode.Impulse);
+        }
+    }
+}
