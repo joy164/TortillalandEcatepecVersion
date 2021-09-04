befca793626d8e52782c1934cb787c9a13c692ed
joy164 <joelvillar35@gmail.com>
2021-09-04 02:06:25 -0500

    colision reparada y modelos separados

--- New Unity Project/Assets/scripts/moviminetos.cs
+++ New Unity Project/Assets/scripts/moviminetos.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class moviminetos : MonoBehaviour{
-    public float speed = 10.0f, gravedad = 10.0f;
+    public float speed = 10.0f, fuerzaSalto = 10.0f;
     
     private Rigidbody Physics;
     // Start is called before the first frame update
@@ -18,13 +18,13 @@ public class moviminetos : MonoBehaviour{
         float horizontal = Input.GetAxis("Horizontal");
         
         
-        transform.Translate(new Vector3(horizontal, 0.0f, 0.0f) * Time.deltaTime * speed);
+        transform.Translate(new Vector3(horizontal, 0.0f, 0.0f) * Time.deltaTime * speed * -1);
         
         /*float rotacionCamara = Input.GetAxis("Mouse Y");
         transform.Rotate(new Vector3( 0.0f, 0.0f, rotacionCamara * Time.deltaTime * rotacion));*/
 
         if(Input.GetKeyDown(KeyCode.Space)){
-            Physics.AddForce(new Vector3(0,gravedad,0), ForceMode.Impulse);
+            Physics.AddForce(new Vector3(0,fuerzaSalto,0), ForceMode.Impulse);
         }
     }
 }
